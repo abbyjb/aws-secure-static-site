efe0e8ed57a91e5fdf863ce61f032f7c76b414d6
abbyjb <22553870+abbyjb@users.noreply.github.com>
2025-11-17 19:38:42 -0800

    support assume_role

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "domain" {
   description = "domain name of your website"
   type        = string
 }
+
+variable "role_arn" {
+  description = "Role ARN"
+  type        = string
+}
+
+variable "external_id" {
+  description = "External ID"
+  type        = string
+}
